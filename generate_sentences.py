b5c130318187e3f5e943b3b7814d65bef21ee789
bryant <bryantyw@cs.washington.edu>
2015-12-28 18:49:05 -0800

    remove private key, whoops

--- generate_sentences.py
+++ generate_sentences.py
@@ -15,7 +15,7 @@ def generate_trigrams(corpus, filepath):
                   (as confirmation)
 	'''
 	input = [corpus, "xxBeGiN142xx", "xxEnD142xx", filepath]
-	client = Algorithmia.client('simfAKlzXJA516uRJm37b8tT9b31')
+	client = Algorithmia.client('')
 	algo = client.algo('ngram/GenerateTrigramFrequencies/0.1.1')
 	print "Trigram Frequency txt in data api, filepath is:"
 	print algo.pipe(input)
@@ -30,7 +30,7 @@ def generate_sentence(filepath):
     	<str> output: a randomly generated sentence
 	'''
 	input = [filepath, "xxBeGiN142xx", "xxEnD142xx"]
-	client = Algorithmia.client('simfAKlzXJA516uRJm37b8tT9b31')
+	client = Algorithmia.client('')
 	algo = client.algo('ngram/RandomTextFromTrigram/0.1.1')
 	print algo.pipe(input)
 
